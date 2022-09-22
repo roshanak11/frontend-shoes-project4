7cf2c77138eac46897628b6ca861fc6f89e98062
roshanak <roshanak.ahmed11@gmail.com>
2022-09-22 16:09:13 +0600

    mobile media for shoe index

--- src/components/ShoeIndex.js
+++ src/components/ShoeIndex.js
@@ -17,24 +17,26 @@ function ShoeIndex() {
 
   return (
     <div className="section">
-      <h1 className="title">Shoes</h1>
+      <h1 className="title has-text-centered">Shoes</h1>
       <div className="columns is-multiline is-mobile">
         {shoe.map(shoe => {
           return (
-            <>
-              <Link to={`/shoes/${shoe.id}`}>
-                <div className="card">
-                  <div className="card-content">
-                    <h2 key={shoe.name}>{shoe.name} | £{shoe.price}</h2>
+            <div key={shoe} className="column is-one-quarter-desktop is-third-tablet is-half-mobile">
+              <>
+                <Link to={`/shoes/${shoe.id}`}>
+                  <div className="card">
+                    <div className="card-content">
+                      <h2 key={shoe.name}>{shoe.name} | £{shoe.price}</h2>
+                    </div>
                   </div>
-                </div>
-                <div className="card-image">
-                  <figure className="image is-1by1">
-                    <img src={shoe.image} alt={shoe.name} />
-                  </figure>
-                </div>
-              </Link>
-            </>
+                  <div className="card-image">
+                    <figure className="image is-1by1">
+                      <img src={shoe.image} alt={shoe.name} />
+                    </figure>
+                  </div>
+                </Link>
+              </>
+            </div>
           )
         })}
       </div>
