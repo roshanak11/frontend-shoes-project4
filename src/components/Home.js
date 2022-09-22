ddcc339996a4327f546edf6d6907392d8974b5b2
roshanak <roshanak.ahmed11@gmail.com>
2022-09-22 15:04:17 +0600

    home-page CSS & divs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,9 @@
 function Home() {
   return (
     <section className="hero is-fullheight-with-navbar is-warning">
-      <div className="hero-body">
-        <div className="container">
-          <p className="title is-1 has-text-centered has-text-black">
-            Shoe Drip
-          </p>
-          <div className="head-image">
-            <img src={('https://res.cloudinary.com/dgicm5dgb/image/upload/v1661385128/project-4-shoes/homepage2_onvdcq.webp')} alt="Shoe Drip" />
-          </div>
+      <div id="homepage-background">
+        <div id="homepage">
+          <h2 id="title">Shoe Drip</h2>
         </div>
       </div>
     </section>
